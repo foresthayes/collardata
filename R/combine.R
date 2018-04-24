0f59f0fea7b8812d7a0d4374824387c376f9b781
Forest Hayes <forest.hayes@umontana.edu>
2018-04-24 12:29:25 -0600

    Changed combine function
    Changed combine function to work with .txt files and simplify operation.

--- R/combine.R
+++ R/combine.R
@@ -1,21 +1,23 @@
 #' Combine CSV files
 #'
 #' @param data_folder Folder that CSV files are contained within
+#' @param file_type Extensions of files to combine.  Default is .txt and .csv
+#' @param sep Delimiter of data.  Defailt is "," i.e. for .csv files
 #'
 #' @return Returns a data table of unique rows (removes duplicates)
 #' @export
 #'
 #' @examples ATS_collar_data <- combine("data/ATS")
-combine <- function(data_folder = "data"){
+combine <- function(data_folder = "data", file_type = c("csv", "txt"), sep = ","){
 
   # List files within "data_folder" directory
-  file_list <- dir(here::here(data_folder))
+  file_list <- list.files(
+    here::here(data_folder),
+    pattern = file_type,
+    full.names = T)
 
-  # Set directory to data_folder location
-  setwd(here::here(data_folder))
-
-  # Combine csv files into single data frame
-  combined_data <- do.call(rbind,lapply(file_list,read.csv)) # Possible to change to read.table to handle .txt or .csv?
+  # Combine files into single data frame
+  combined_data <- do.call(rbind,lapply(file_list,read.delim, sep = sep)) # Possible to change to read.table to handle .txt or .csv?
 
   # Return only unique rows
   unique(combined_data[1:length(combined_data[1, ])])
