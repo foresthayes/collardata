6029ab6a75e57817e44ab0821360588c93c7a74a
Forest Hayes <forest.hayes@umontana.edu>
2018-09-09 22:55:58 -0600

    Bug fixes

--- R/read_database.R
+++ R/read_database.R
@@ -7,10 +7,10 @@
 #'
 #' @examples read_database("data/ats")
 
-read_database <- function(data_folder = NA, colClasses = NULL){
+read_database <- function(data_folder = NA, colClasses = NA){
 
   # Identify the most recent database file
-  database_file <- tail(list.files(here::here(data_folder), pattern = "csv", full.names = T))
+  database_file <- tail(list.files(here::here(data_folder), pattern = "csv", full.names = T), 1)
 
   # Read in the latest database file
   database <- read.delim(database_file, sep = ",", colClasses = colClasses)
